7dfe2488f67bd112011494e26056a4e592de8975
Dinis <dinisde@gmail.com>
2023-12-09 14:53:44 -0300

    consertei entrada do usuario

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pygame
 
 
 path_audio = '/home/dinis/game/jokenpo/audios/'
-
+jogadas = [0,1,2]
 
 pygame.mixer.init()
 cont = "1"
@@ -21,93 +21,97 @@ while cont == "1":
     pygame.mixer.music.play()
     jogador = int(
         input(f'\nEscolha uma alternativa:\n{negrito}{cor_vermelha}Pedra [0] Papel [1] Tesoura [2] ->>{cor_reset} '))
-    pygame.mixer.music.stop()
-    print("=-" * 20)
-    pygame.mixer.music.load(f"{path_audio}boom-geomorphism-cinematic-trailer-sound-effects-123876.mp3")
-    pygame.mixer.music.play()
-    print(f'{negrito}{cor_amarela}J0')
-    time.sleep(1)
-    pygame.mixer.music.stop()
-    pygame.mixer.music.load(f"{path_audio}boom-geomorphism-cinematic-trailer-sound-effects-123876.mp3")
-    pygame.mixer.music.play()
-    print("KEN")
-    time.sleep(1)
-    pygame.mixer.music.stop()
-    pygame.mixer.music.load(f"{path_audio}boom-geomorphism-cinematic-trailer-sound-effects-123876.mp3")
-    pygame.mixer.music.play()
-    print(f'PO{cor_reset}\n')
-    time.sleep(1)
-    pygame.mixer.music.stop()
-    cpu = random.randrange(0, 3)
-    print("=-" * 20)
-    pygame.mixer.music.load(f"{path_audio}boom-geomorphism-cinematic-trailer-sound-effects-123876.mp3")
-    pygame.mixer.music.play()
-    print(f'{cor_verde}VOCE JOGOU = {cor_azul}{joquem[jogador]}')
-    time.sleep(1)
-    pygame.mixer.music.stop()
-    pygame.mixer.music.load(f"{path_audio}boom-geomorphism-cinematic-trailer-sound-effects-123876.mp3")
-    pygame.mixer.music.play()
-    print(f'{cor_verde}CPU  JOGOU = {cor_azul}{joquem[cpu]}\n')
-    time.sleep(1)
-    pygame.mixer.music.stop()
+    if jogador in jogadas:  
+        pygame.mixer.music.stop()
+        print("=-" * 20)
+        pygame.mixer.music.load(f"{path_audio}boom-geomorphism-cinematic-trailer-sound-effects-123876.mp3")
+        pygame.mixer.music.play()
+        print(f'{negrito}{cor_amarela}J0')
+        time.sleep(1)
+        pygame.mixer.music.stop()
+        pygame.mixer.music.load(f"{path_audio}boom-geomorphism-cinematic-trailer-sound-effects-123876.mp3")
+        pygame.mixer.music.play()
+        print("KEN")
+        time.sleep(1)
+        pygame.mixer.music.stop()
+        pygame.mixer.music.load(f"{path_audio}boom-geomorphism-cinematic-trailer-sound-effects-123876.mp3")
+        pygame.mixer.music.play()
+        print(f'PO{cor_reset}\n')
+        time.sleep(1)
+        pygame.mixer.music.stop()
+        cpu = random.randrange(0, 3)
+        print("=-" * 20)
+        pygame.mixer.music.load(f"{path_audio}boom-geomorphism-cinematic-trailer-sound-effects-123876.mp3")
+        pygame.mixer.music.play()
+        print(f'{cor_verde}VOCE JOGOU = {cor_azul}{joquem[jogador]}')
+        time.sleep(1)
+        pygame.mixer.music.stop()
+        pygame.mixer.music.load(f"{path_audio}boom-geomorphism-cinematic-trailer-sound-effects-123876.mp3")
+        pygame.mixer.music.play()
+        print(f'{cor_verde}CPU  JOGOU = {cor_azul}{joquem[cpu]}\n')
+        time.sleep(1)
+        pygame.mixer.music.stop()
 
-    if cpu == 0:
-        if jogador == 1:
-            pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
-            pygame.mixer.music.play()
-            print(f'{negrito}{cor_azul}VOCE VENCEU!!!{cor_reset}')
-            time.sleep(4)
-            pygame.mixer.music.stop()
-        elif jogador == 2:
-            pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
-            pygame.mixer.music.play()
-            print(f'{negrito}{cor_azul}CPU VENCEU!!!{cor_reset}')
-            time.sleep(4)
-            pygame.mixer.music.stop()
-        else:
-            pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
-            pygame.mixer.music.play()
-            print(f'{negrito}{cor_azul}EMPATE!!!{cor_reset}')
-            time.sleep(4)
-            pygame.mixer.music.stop()
-    elif cpu == 1:
-        if jogador == 0:
-            pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
-            pygame.mixer.music.play()
-            print(f'{negrito}{cor_azul}CPU VENCEU!!!{cor_reset}')
-            time.sleep(4)
-            pygame.mixer.music.stop()
-        elif jogador == 2:
-            pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
-            pygame.mixer.music.play()
-            print(f'{negrito}{cor_azul}VOCE VENCEU!!!{cor_reset}')
-            time.sleep(4)
-            pygame.mixer.music.stop()
-        else:
-            pygame.mixer.music.load(f"{path_audio}/epic-hybrid-logo-157092.mp3")
-            pygame.mixer.music.play()
-            print(f'{negrito}{cor_azul}EMPATE!!!{cor_reset}')
-            time.sleep(4)
-            pygame.mixer.music.stop()
-    elif cpu == 2:
-        if jogador == 1:
-            pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
-            pygame.mixer.music.play()
-            print(f'{negrito}{cor_azul}CPU VENCEU!!!{cor_reset}')
-            time.sleep(4)
-            pygame.mixer.music.stop()
-        elif jogador == 0:
-            pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
-            pygame.mixer.music.play()
-            print(f'{negrito}{cor_azul}VOCE VENCEU!!!{cor_reset}')
-            time.sleep(4)
-            pygame.mixer.music.stop()
-        else:
-            pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
-            pygame.mixer.music.play()
-            print(f'{negrito}{cor_azul}EMPATE!!!{cor_reset}')
-            time.sleep(4)
-            pygame.mixer.music.stop()
+        if cpu == 0:
+            if jogador == 1:
+                pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
+                pygame.mixer.music.play()
+                print(f'{negrito}{cor_azul}VOCE VENCEU!!!{cor_reset}')
+                time.sleep(4)
+                pygame.mixer.music.stop()
+            elif jogador == 2:
+                pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
+                pygame.mixer.music.play()
+                print(f'{negrito}{cor_azul}CPU VENCEU!!!{cor_reset}')
+                time.sleep(4)
+                pygame.mixer.music.stop()
+            else:
+                pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
+                pygame.mixer.music.play()
+                print(f'{negrito}{cor_azul}EMPATE!!!{cor_reset}')
+                time.sleep(4)
+                pygame.mixer.music.stop()
+        elif cpu == 1:
+            if jogador == 0:
+                pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
+                pygame.mixer.music.play()
+                print(f'{negrito}{cor_azul}CPU VENCEU!!!{cor_reset}')
+                time.sleep(4)
+                pygame.mixer.music.stop()
+            elif jogador == 2:
+                pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
+                pygame.mixer.music.play()
+                print(f'{negrito}{cor_azul}VOCE VENCEU!!!{cor_reset}')
+                time.sleep(4)
+                pygame.mixer.music.stop()
+            else:
+                pygame.mixer.music.load(f"{path_audio}/epic-hybrid-logo-157092.mp3")
+                pygame.mixer.music.play()
+                print(f'{negrito}{cor_azul}EMPATE!!!{cor_reset}')
+                time.sleep(4)
+                pygame.mixer.music.stop()
+        elif cpu == 2:
+            if jogador == 1:
+                pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
+                pygame.mixer.music.play()
+                print(f'{negrito}{cor_azul}CPU VENCEU!!!{cor_reset}')
+                time.sleep(4)
+                pygame.mixer.music.stop()
+            elif jogador == 0:
+                pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
+                pygame.mixer.music.play()
+                print(f'{negrito}{cor_azul}VOCE VENCEU!!!{cor_reset}')
+                time.sleep(4)
+                pygame.mixer.music.stop()
+            else:
+                pygame.mixer.music.load(f"{path_audio}epic-hybrid-logo-157092.mp3")
+                pygame.mixer.music.play()
+                print(f'{negrito}{cor_azul}EMPATE!!!{cor_reset}')
+                time.sleep(4)
+                pygame.mixer.music.stop()
+    else:
+        print('Jogada invalida')
+        cont = 1   
 
     print("=-" * 20)
     pygame.mixer.music.load(f"{path_audio}Jellica - I tried to write some cutesy platformer kiddy nintendo shit.mp3")
